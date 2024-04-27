15dcc991c806412ea21ffb1de3d134f6c027cf1d
Max Lehmann <85064456+MaxLehmann01@users.noreply.github.com>
2024-04-27 19:53:15 +0200

    Update Projects.tsx

--- src/pages/Content/Projects.tsx
+++ src/pages/Content/Projects.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { tProject } from "../../types/tProject";
 import { AxiosError } from "axios";
 import useLocalStorage from "../../hooks/useLocalStorage";
@@ -8,21 +8,37 @@ import API from "../../utils/API";
 const Projects = () => {
   const { language } = useLocalStorage();
   const { setAlert } = useAlert();
+  const retryIntervalRef = useRef<number | null>(null);
 
   const [ projects, setProjects ] = useState<tProject[]>([]);
 
   const fetchProjects = async () => {
     try {
       const response = await API.get('/projects');
-      setProjects(response.data as tProject[])
+      setProjects(response.data as tProject[]);
+      if (retryIntervalRef.current) {
+        clearInterval(retryIntervalRef.current);
+        retryIntervalRef.current = null;
+      }
     } catch (err) {
       const error = err as AxiosError;
-      setAlert({ severity: 'error', children: `${language === 'de' ? 'Die Projekte konnten nicht geladen werden!' : 'The Projects could not be loaded!'} - Status ${error.response?.status}` })
+      setAlert({ severity: 'error', children: `${language === 'de' ? 'Die Projekte konnten nicht geladen werden!' : 'The Projects could not be loaded!'} - Status: ${error.response?.status}` })
     }
   }
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(() => { fetchProjects(); }, [])
+  useEffect(() => { 
+    fetchProjects();
+
+    retryIntervalRef.current = setInterval(() => {
+      fetchProjects();
+    }, 5000);
+    
+    return () => {
+      if(retryIntervalRef.current) clearInterval(retryIntervalRef.current)
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, [])
 
   return (
     <div>
